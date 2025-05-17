595cc9ad1514894df59ed627c1636e6269464326
AlexisMendez34 <166565669+AlexisMendez34@users.noreply.github.com>
2025-05-16 22:01:48 -0700

    Se cargaron la instrucciones generales del proyecto

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace MatrixIMG
     {
         static void Main(string[] args)
         {
+            //Se debe realizar una función que pueda leer una imagen monocromática y convertirla a una matriz de píxeles, La función debe devolver la matriz de píxeles.
+            //Se requieren realizar las funciones que hagan la transpuesta, y la reversa transpuesta de dicha matriz.
+            //Despues se debe crear una imagen a partir de la matriz de píxeles, y guardar la imagen en un archivo.
+            //Por ultimo, se debe realizar una interfaz que permita al usuario elegir que proceso realizar.
         }
     }
 }
