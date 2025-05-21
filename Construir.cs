5055fd1d487f8834a56a0a6fcda131967fe8063d
Tess <tssmirsalruso@gmail.com>
2025-05-21 10:18:14 -0700

    CodeTSS

--- Construir.cs
+++ Construir.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace MatrixIMG
 {
     class Construir
     {
+        public void GuardarImagenDesdeMatriz(Color[,] matriz, string rutaArchivo)
+        {
+            int ancho = matriz.GetLength(0);
+            int alto = matriz.GetLength(1);
+
+            using (Bitmap imagen = new Bitmap(ancho, alto))
+            {
+                for (int y = 0; y < alto; y++)
+                {
+                    for (int x = 0; x < ancho; x++)
+                    {
+                        imagen.SetPixel(x, y, matriz[x, y]);
+                    }
+                }
+
+                imagen.Save(rutaArchivo, System.Drawing.Imaging.ImageFormat.Png);
+                Console.WriteLine($"Imagen guardada en: {rutaArchivo}");
+            }
+        }
     }
 }
