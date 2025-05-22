b50ec9776f2e10cc3bef1f09c95e8ff3f601b424
emij3ele <emilianorosas1606@gmail.com>
2025-05-21 22:51:29 -0700

    ooo

--- Modificar.cs
+++ Modificar.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MatrixIMG
 {
-    class Modificar
+    class Modificar : Extraer
     {
+        Bitmap imgBit = new Bitmap("../../imagenes/casillas.png");
+
+
+        public void Transpuesta(int[,] modMatrix)
+        {
+            int tamImg = imgBit.Width;
+
+            for (int j = 0; j < tamImg; j++)
+            {
+                for (int i = 0; i < tamImg; i++)
+                {
+                    Color color = imgBit.GetPixel(j, i);
+                    int pixel = (color.R + color.G + color.B) / 3;
+                    modMatrix[j, i] = pixel;
+                    char pix = Renderizar(pixel);
+                    Console.Write(pix + " ");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\nTranspuesta lista\n\n");
+        }
+
+        public void TranspuestaInversa(int[,] modMatrix)
+        {
+            int tamImg = imgBit.Width;
+
+            for (int j = tamImg - 1; j >= 0; j--)
+            {
+                for (int i = tamImg - 1; i >= 0; i--)
+                {
+                    Color color = imgBit.GetPixel(j, i);
+                    int pixel = (color.R + color.G + color.B) / 3;
+                    modMatrix[j, i] = pixel;
+                    char pix = Renderizar(pixel);
+                    Console.Write(pix + " ");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\nTranspuesta e inversa lista\n\n");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -16,17 +16,20 @@ namespace MatrixIMG
 
             int[,] matrix = extractor.ExtIMG();
 
+            operacion.Transpuesta(matrix);
+            operacion.TranspuestaInversa(matrix);
+
+            Construir.Cuadriculado cuadriculado = new Construir.Cuadriculado(8, 10); // 8x8 cuadros de 10 píxeles
+
+
+            construccion.GuardarImagenDesdeMatriz("../../imagenes/cuadricula.png");
+
             Console.WriteLine("Presione cualquier tecla para salir");
             Console.ReadKey();
 
             //ALEXIS Se debe realizar una función que pueda leer una imagen monocromática y convertirla a una matriz de píxeles, La función debe devolver la matriz de píxeles.
             //IAN Se requieren realizar las funciones que hagan la transpuesta, y la reversa transpuesta de dicha matriz.
             //TESS Despues se debe crear una imagen a partir de la matriz de píxeles, y guardar la imagen en un archivo.
-
-            Construir.Cuadriculado cuadriculado = new Construir.Cuadriculado(8, 10); // 8x8 cuadros de 10 píxeles
-
-            
-            construccion.GuardarImagenDesdeMatriz("../../imagenes/cuadricula.png");
             
             //Por ultimo, se debe realizar una interfaz que permita al usuario elegir que proceso realizar.
         }
