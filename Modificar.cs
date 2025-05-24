cda611f283b1662fd3a03ef36c7c7f9637e67c54
emij3ele <emilianorosas1606@gmail.com>
2025-05-23 18:23:32 -0700

    Las funciones de transpuesta y trnaspuesta/inversa devuelven la matriz, igual que la función de agregar ruido, esto para ir modificando matrix en el program, además de que ya no utilizan un objeto BitMap para obtener el tamaño de la imagen sino utilizando las dimensiones del arreglo. Ahora las funciones de transpuesta y transpuesta/inversa solamente generan la imagen en consola mediante la matriz que se pasa por parametros.

--- Modificar.cs
+++ Modificar.cs
@@ -10,45 +10,103 @@ namespace MatrixIMG
 {
     class Modificar : Extraer
     {
-        Bitmap imgBit = new Bitmap("../../imagenes/casillas.png");
 
-
-        public void Transpuesta(int[,] modMatrix)
+        public int[,] Transpuesta(int[,] modMatrix) 
         {
-            int tamImg = imgBit.Width;
 
-            for (int j = 0; j < tamImg; j++)
+            for (int j = 0; j < modMatrix.GetLength(1); j++) // El arreglo se recorre por cada renglon y después sigue a la siguiente columna
             {
-                for (int i = 0; i < tamImg; i++)
+                for (int i = 0; i < modMatrix.GetLength(0); i++)
                 {
-                    Color color = imgBit.GetPixel(j, i);
-                    int pixel = (color.R + color.G + color.B) / 3;
-                    modMatrix[j, i] = pixel;
+                    int pixel = modMatrix[j, i];
                     char pix = Renderizar(pixel);
                     Console.Write(pix + " ");
                 }
                 Console.Write("\n");
             }
             Console.Write("\nTranspuesta lista\n\n");
+
+            Console.Write("Presione enter para continuar...");
+            Console.ReadKey();
+
+            return modMatrix;
         }
 
-        public void TranspuestaInversa(int[,] modMatrix)
+        public int[,] TranspuestaInversa(int[,] modMatrix)
         {
-            int tamImg = imgBit.Width;
 
-            for (int j = tamImg - 1; j >= 0; j--)
+            int tamImg = modMatrix.Length;
+
+            for (int j = modMatrix.GetLength(1) - 1; j >= 0; j--) // Igual que la transpuesta solamente que el arreglo empieza desde el último elemento del arreglo
             {
-                for (int i = tamImg - 1; i >= 0; i--)
+                for (int i = modMatrix.GetLength(0) - 1; i >= 0; i--)
                 {
-                    Color color = imgBit.GetPixel(j, i);
-                    int pixel = (color.R + color.G + color.B) / 3;
-                    modMatrix[j, i] = pixel;
+                    int pixel = modMatrix[j, i];
                     char pix = Renderizar(pixel);
                     Console.Write(pix + " ");
                 }
                 Console.Write("\n");
             }
             Console.Write("\nTranspuesta e inversa lista\n\n");
+
+            Console.Write("Presione enter para continuar...");
+            Console.ReadKey();
+
+            return modMatrix;
+        }
+
+        public int[,] GenerarRuido(int[,] modMatrix)
+        {
+
+            int tamImg = modMatrix.Length;
+            Random rnd = new Random();
+
+            Console.Write("Ingrese el porcentaje de ruido deseado (0-1): ");
+
+            try
+            {
+                double porcentajeRuido = double.Parse(Console.ReadLine());
+
+                if (porcentajeRuido < 0 || porcentajeRuido > 1)
+                    throw new ArgumentOutOfRangeException("Ingresa un porcentaje de ruido adecuado");
+
+                for (int i = 0; i < modMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < modMatrix.GetLength(1); j++)
+                    {
+                        int pixel = modMatrix[j, i];
+
+                        if (rnd.NextDouble() < porcentajeRuido)
+                        {
+                            int ruido = rnd.Next(-50, 51); // Genera un ruido entre -50 y 50
+                            pixel += ruido;
+                            pixel = Math.Max(0, Math.Min(255, pixel)); // Asegura que pixel no salga del rango de [0, 255]
+                        }
+
+                        modMatrix[j, i] = pixel;
+                    }
+                }
+                Console.Write("\nEl ruuido agregado correctamente\n\n");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Write("Presione enter para continuar...");
+                Console.ReadKey();
+            }
+
+            return modMatrix;
         }
     }
 }
