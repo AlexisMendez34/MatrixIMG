01d2331d4565a2aaae4697c76de9261e0fbb7c22
AlexisMendez34 <166565669+AlexisMendez34@users.noreply.github.com>
2025-05-23 20:59:27 -0700

    Manejo de errores en la construcción de la imagen

--- Construir.cs
+++ Construir.cs
@@ -7,19 +7,24 @@ namespace MatrixIMG
     {
         public void GuardarImagenDesdeMatriz(string rutaArchivo, int[,] matriz)
         {
-            int size = matriz.GetLength(0);
+            if (matriz.GetLength(0) == 0) return;
 
-            for (int i = 0; i < size; i++)
+            else
             {
-                for (int j = 0; j < size; j++)
-                {
-                    using (Bitmap imagen = new Bitmap(size, size))
-                    {
-                        imagen.SetPixel(j, i, Color.FromArgb(matriz[j, i], matriz[j, i], matriz[j, i]));
+                int size = matriz.GetLength(0);
 
-                        imagen.Save(rutaArchivo, System.Drawing.Imaging.ImageFormat.Png);
-                        Console.WriteLine($"Imagen guardada en: {rutaArchivo}");
-                        Console.ReadKey();
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        using (Bitmap imagen = new Bitmap(size, size))
+                        {
+                            imagen.SetPixel(j, i, Color.FromArgb(matriz[j, i], matriz[j, i], matriz[j, i]));
+
+                            imagen.Save(rutaArchivo, System.Drawing.Imaging.ImageFormat.Png);
+                            Console.WriteLine($"Imagen guardada en: {rutaArchivo}");
+                            Console.ReadKey();
+                        }
                     }
                 }
             }
